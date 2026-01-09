0c3350f6209caa9ce26aea12b8f88c8638d570eb
Vaishnavi Deshmukh <dvaishnavi028@gmail.com>
2026-01-09 16:08:27 +0530

    initial comment

---
+++ src/basic/EvenOdd.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		int num = 74;
+		
+		if(num%2 == 0) {
+			System.out.println("It an even number");
+		}
+		else {
+			System.out.println("Its an odd number!");
+		}
+		
+	}
+	
+}
